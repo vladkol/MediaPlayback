2d8a751f1c36dc4492253fca50d411e6832df95b
Vlad Kolesnikov <vladkol@microsoft.com>
2017-08-10 08:52:50 -0700

    Copied changed material and demo scene to the main package

---
+++ MediaPlayback/Unity/MediaPlayback/Assets/Addons/MediaPlayer/MediaPlaybackRunner.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using UnityEngine;
+
+[RequireComponent(typeof(MediaPlayer.Playback))]
+public class MediaPlaybackRunner : MonoBehaviour {
+
+    public string mediaURI = string.Empty;
+    public bool autoPlay = true;
+
+    MediaPlayer.Playback _player;
+
+    private void Awake()
+    {
+        _player = GetComponent<MediaPlayer.Playback>();
+    }
+
+    // Use this for initialization
+    void Start () {
+		if(autoPlay)
+        {
+            Play();
+        }
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void Play()
+    {
+        if(!string.IsNullOrEmpty(mediaURI))
+        {
+            string uriStr = mediaURI;
+
+            if (Uri.IsWellFormedUriString(mediaURI, UriKind.Absolute))
+            {
+                uriStr = mediaURI;
+            }
+            else if (Path.IsPathRooted(mediaURI))
+            {
+                uriStr = "file:///" + mediaURI;
+            }
+            else
+            {
+                uriStr = "file:///" + Path.Combine(Application.streamingAssetsPath, mediaURI);
+            }
+
+            _player.Play(uriStr);
+        }
+    }
+
+    public MediaPlayer.Playback GetMediaPlayer()
+    {
+        return _player;
+    }
+}
